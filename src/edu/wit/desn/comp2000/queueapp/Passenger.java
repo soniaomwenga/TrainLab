471313fdb139cbaaf4ce9c1c2e00aac1f02da2e3
Sonia Omwenga <omwengas@wit.edu>
2017-10-24 10:11:14 -0400

    sonia's latest

--- src/edu/wit/desn/comp2000/queueapp/Passenger.java
+++ src/edu/wit/desn/comp2000/queueapp/Passenger.java
@@ -2,40 +2,97 @@ package edu.wit.desn.comp2000.queueapp;
 
 public class Passenger
 {
-private int ID;
-private int destinationID;
-private int arrivalID;
+	private int ID;
+	private int destinationID;
+	private int arrivalID;
 
-public Passenger (int ID1, int destinationID1, int arrivalID1)
-{
-ID = ID1;
-destinationID = destinationID1;
-arrivalID = arrivalID1;
-	
-}
+	public Passenger(int ID1, int destinationID1, int arrivalID1)
+	{
+		ID = ID1;
+		destinationID = destinationID1;
+		arrivalID = arrivalID1;
 
+	}
 
-public int getID()
-{
-	return ID;
-}
+	/**
+	 * this method will get the ID passengers ID to use it in other classes 
+	 * such as station 
+	 * @return
+	 */
+	public int getID()
+	{
+		return ID;
+	}
 
-public int getDestinationID()
-{
-	return destinationID;
-}
+	/**
+	 * This method is to get the destination station number so that 
+	 * the station class will know where to let passengers off 
+	 * @return
+	 */
+	public int getDestinationID()
+	{
+		return destinationID;
+	}
 
-public int getArrivalID()
-{
-	return arrivalID;
-}
+	/**
+	 *  This method is to get the arrival station number so that 
+	 * the station class will know how to plan the route of the passenger
+	 * and which platform (queue) to add them to. 
+	 * @return
+	 */
+	public int getArrivalID()
+	{
+		return arrivalID;
+	}
 
-public boolean check(int destination, int arrival)
-{
-	if (destination != arrival)
-		return false;
-	else 
+	/**
+	 * boolean method to make sure the random passenger generator 
+	 * does not accidentally set the passengers arrival station equal 
+	 * to their destination on but checking this before inserting the 
+	 * passenger into the simulation. 
+	 * @param destination
+	 * @param arrival
+	 * @return
+	 */
+	public boolean check(int destination, int arrival)
+	{
+		if (destination != arrival)
+			return false;
+		else
+			return true;
+
+	}
+
+	////////////////////// TESTER METHODS BELOW//////////////////////////////
+	/**
+	 * Test that the ID assigned to a passenger has not 
+	 * 	-already been assigned to another passenger 
+	 * 	- is not less than 1 
+	 * 	- is not greater than the maximum number of passengers 
+	 * 		outlined in the config file 
+	 * @return
+	 */
+	public boolean testGetID()
+	{
 		return true;
-	
-}
+	}
+
+	/**
+	 * Test to make sure DestinationID is 
+	 * 	- an index of the ArrayList which has a station there  
+	 * @return
+	 */
+	public boolean testGetDestinationID()
+	{
+		return true;
+	}
+	/**
+	 * Test to make sure ArrivalID is 
+	 * 	- an index of the ArrayList which has a station there  
+	 * @return
+	 */
+	public boolean testGetArrivalID()
+	{
+		return true;
+	}
 }
--- src/edu/wit/desn/comp2000/queueapp/TrainRoute.java
+++ src/edu/wit/desn/comp2000/queueapp/TrainRoute.java
@@ -1,8 +1,32 @@
 package edu.wit.desn.comp2000.queueapp;
 
+import java.util.ArrayList;
+
+import com.pearson.carrano.ArrayQueue;
+
 public class TrainRoute
 {
-
+	private ArrayList<Train> inboundTracks = new ArrayList<Train>(30);
+	private ArrayList<Train> outboundTracks = new ArrayList<Train>(30);
+	private int location;
+	private int destinationID;
+	private int arrivalID;
 	
+	public TrainRoute (int arrivalID1, int destinationID1)
+	{
+		destinationID = destinationID1;
+		arrivalID = arrivalID1;
+	}
+	/**
+	 * this method will reverse the direction of 
+	 * any train which has reached the end of the route. 
+	 * @param location
+	 * @return
+	 */
+	public int reverseDirection(int location)
+	{
+		return location;
+		
+	}
 	
 }
