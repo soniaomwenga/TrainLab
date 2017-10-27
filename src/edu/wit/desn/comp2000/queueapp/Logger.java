7892437e0a7ca63fd03c57dcd91078c3dbd2769c
garciamathurinj <garciamathurinj@localhost>
2017-10-26 20:03:42 -0400

    logger update

--- src/edu/wit/desn/comp2000/queueapp/Logger.java
+++ src/edu/wit/desn/comp2000/queueapp/Logger.java
@@ -1,8 +1,43 @@
 package edu.wit.desn.comp2000.queueapp;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+
+
 
 public class Logger
 {
 // all variable here will be private static 
 //methods here will take the strings passed to it and log them into the file called logger. 
+	private static PrintWriter pw;
 	
-}
+	//a//
+	public static void write(String message){
+		pw.println(message);
+	
+	}
+		
+	public static void Create(){
+		try {
+			File file = new File(("TrainSimulation.log"));
+
+			pw = new PrintWriter(file);
+		} catch(FileNotFoundException e) {
+		e.printStackTrace();
+		}
+	}
+		
+	public static void close(){
+	
+	pw.close();
+	
+	}
+		
+
+		
+		
+
+		
+	
+	}
+	
